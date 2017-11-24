e605a93227c85989efe1aa2e8475616b401ab772
htcvtc59 <htcvtc59@gmail.com>
2017-11-24 13:09:12 +0700

    webhook

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = "EAAB4z2lVMRMBAJ9vbZC8C5RODwpKRCHHOZBxIZBWOpaKo9oq8FnGVXGRfqFAoEfCujC6YgZB71NqulqPdf1ZB93AwzavQHC3mBLJZBSAIcJzbHa81vClixWHdBpW6zDSTBWIQTs6ta5r8kygPhh0fuJkVNsdE1bzMxFgZB6hKoBF5osJML6lBab";
 
 const
     request = require('request'),
@@ -13,54 +13,86 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello webhook</h1>');
 });
 
+// Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
+
+    // Parse the request body from the POST
     let body = req.body;
+
+    // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
-        body.entry.forEach((entry) => {
+
+        body.entry.forEach(function (entry) {
+
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
+
+            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender ID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
             if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
-                handleMessage(sender_psid, webhook_event.postback);
+
+                handlePostback(sender_psid, webhook_event.postback);
             }
 
         });
+        // Return a '200 OK' response to all events
         res.status(200).send('EVENT_RECEIVED');
+
     } else {
+        // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
+
 });
 
+// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
+
+    /** UPDATE YOUR VERIFY TOKEN **/
     const VERIFY_TOKEN = "132832220754195";
+
+    // Parse params from the webhook verification request
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
+    // Check if a token and mode were sent
     if (mode && token) {
+
+        // Check the mode and token sent are correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Respond with 200 OK and challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
+
         } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
     }
-
 });
 
-
-
-let handleMessage = (sender_psid, received_message) => {
+function handleMessage(sender_psid, received_message) {
     let response;
+
+    // Checks if the message contains text
     if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
             "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
         }
     } else if (received_message.attachments) {
+        // Get the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
             "attachment": {
@@ -71,59 +103,64 @@ let handleMessage = (sender_psid, received_message) => {
                         "title": "Is this the right picture?",
                         "subtitle": "Tap a button to answer.",
                         "image_url": attachment_url,
-                        "buttons": [{
-                            "type": "postback",
-                            "title": "Yes!",
-                            "payload": "yes",
-                        },
-                        {
-                            "type": "postback",
-                            "title": "No!",
-                            "payload": "no",
-                        }],
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
                     }]
                 }
             }
         }
     }
-    callSendAPI(sender_psid, response);
-};
 
-let handlePostback = (sender_psid, received_postback) => {
-    console.log('ok');
+    // Send the response message
+    callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+    console.log('ok')
     let response;
+    // Get the payload for the postback
     let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
     if (payload === 'yes') {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image. " }
+        response = { "text": "Oops, try sending another image." }
     }
+    // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
-};
-let callSendAPI = (sender_psid, response) => {
+}
+
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
         },
         "message": response
     }
+
+    // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "ps": { "access_token": PAGE_ACCESS_TOKEN },
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
         if (!err) {
-            console.log('message send!');
+            console.log('message sent!')
         } else {
-            console.error("Unable to send message: " + err);
+            console.error("Unable to send message:" + err);
         }
     });
-};
-
-
-
-
-
-
-
+}
